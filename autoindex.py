31ebe1cac4e871bf2cbd35f01f381133d025a559
Mahesh Vaidya <mvaidya@facebook.com>
2014-03-30 19:20:41 +0530

    template

--- autoindex.py
+++
@@ -1,9 +0,0 @@
-import os.path
-from flask import Flask
-from flask.ext.autoindex import AutoIndex
-
-app = Flask(__name__)
-AutoIndex(app, browse_root=os.path.curdir)
-
-if __name__ == '__main__':
-    app.run()
